35e5e84edda8eab1a7dc9eb8c712b9ff249df318
Villanueva.!! <Villanueva.!!>
2017-09-18 21:03:57 -0500

    Add project files.

---
+++ Formulario/Formulario/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Formulario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Datos ", Nombre.Text + " " + Apellido.Text + "\n " + Direccion.Text + " \n " + Telefono.Text + " \n" + Carrera.Text + " \n" + Semestre.Text + "\n " + Matricula.Text + "\n " + Correo.Text + "\n " + GitHub.Text, "Aceptar");
+
+        }
+    }
+}
